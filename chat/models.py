d6255b8d3e95af0f561ae890bcf4957cf9cbfc2c
Daniel Shin <shin.daniel.d@gmail.com>
2017-04-07 13:47:36 -0600

    create message models

--- chat/models.py
+++ chat/models.py
@@ -7,3 +7,9 @@ class User(models.Model):
     name = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True,null=True)
+
+class Chat(models.Model):
+    name = models.CharField(max_length=200)
+    message = models.TextField(null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True,null=True)
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.http import JsonResponse
+from .forms import UserForm
+from .models import User
 
 def home(request):
-    return render(request, 'home.html')
+    users = User.objects.all()
+    return render(request, 'home.html', {'users': users })
 
 def users_new(request):
-    return render(request, 'users/new.html')
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.published_date = timezone.now()
+            user.save()
+            return redirect('root')
+    else:
+        form = UserForm()
+    return render(request, 'users/new.html', {'form': form})
